8b8f65eac7ba2609c5421214d94524bb1529fb09
Filipe Masini Santos <filipe.masini@outlook.com>
2023-12-09 00:51:31 -0300

    fix INSERT / UPDATE user address company

--- app.js
+++ app.js
@@ -11,72 +11,139 @@ app.use(bodyParser.json());
 
 // MySQL Connection
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  database: 'counselor',
-  waitForConnections: true,
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'counselor',
+    waitForConnections: true,
 });
 
 // API Routes
 
 // Create User
 app.post('/api/users', async (req, res) => {
-  try {
-    const { name, phone, email, cpf, username, password, address, companies } = req.body;
+    try {
+        const { name, phone, email, cpf, username, password, address, companies } = req.body;
 
-    const saltRounds = 10;
-    const password_hash = await bcrypt.hash(password, saltRounds);
+        const saltRounds = 10;
+        const password_hash = await bcrypt.hash(password, saltRounds);
 
-    // Create Connection from the Pool
+        // Create Connection from the Pool
+        const connection = await pool.getConnection();
+
+        try {
+            await connection.beginTransaction();
+
+            // Create Address
+            const [addressResult] = await connection.query('INSERT INTO address SET ?', [address]);
+            const addressId = addressResult.insertId;
+
+            // Create Companies
+            const companyIds = await Promise.all(companies.map(async company => {
+                const [companyResult] = await connection.query('INSERT INTO company SET ?', [company]);
+                return companyResult.insertId;
+            }));
+
+            // Create User
+            const [userResult] = await connection.query('INSERT INTO user SET ?', {
+                name,
+                phone,
+                email,
+                cpf,
+                username,
+                password_hash,
+            });
+            const userId = userResult.insertId;
+
+            // Update Address and Companies with User ID
+            await connection.query('UPDATE address SET user_id = ? WHERE address_id = ?', [userId, addressId]);
+            await Promise.all(companyIds.map(companyId =>
+                connection.query('UPDATE company SET user_id = ? WHERE company_id = ?', [userId, companyId])
+            ));
+
+            await connection.commit();
+
+            res.status(201).json({ userId, addressId, companyIds });
+        } catch (error) {
+            await connection.rollback();
+            throw error;
+        } finally {
+            connection.release();
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+// Update User
+app.put('/api/users/:userId', async (req, res) => {
     const connection = await pool.getConnection();
 
     try {
-      await connection.beginTransaction();
+        await connection.beginTransaction();
 
-      // Create Address
-      const [addressResult] = await connection.query('INSERT INTO address SET ?', [address]);
-      const addressId = addressResult.insertId;
+        const userId = req.params.userId;
+        const { name, phone, email, cpf, address, companiesToAdd, companiesToRemove } = req.body;
 
-      // Create Companies
-      const companyIds = await Promise.all(companies.map(async company => {
-        const [companyResult] = await connection.query('INSERT INTO company SET ?', [company]);
-        return companyResult.insertId;
-      }));
+        const cpfCheck = await connection.query('SELECT user_id FROM user WHERE cpf = ? AND user_id <> ?', [cpf, userId]);
+        console.log("cpf:" + cpf)
+        if (cpfCheck.userId != null) {
+            await connection.rollback();
+            return res.status(400).json({ error: 'CPF already in use' });
+        }
 
-      // Create User
-      const [userResult] = await connection.query('INSERT INTO user SET ?', {
-        name,
-        phone,
-        email,
-        cpf,
-        username,
-        password_hash,
-      });
-      const userId = userResult.insertId;
+        // Update User
+        await connection.query('UPDATE user SET name = ?, phone = ?, email = ?, cpf = ? WHERE user_id = ?', [name, phone, email, cpf, userId]);
 
-      // Update Address and Companies with User ID
-      await connection.query('UPDATE address SET user_id = ? WHERE address_id = ?', [userId, addressId]);
-      await Promise.all(companyIds.map(companyId =>
-        connection.query('UPDATE company SET user_id = ? WHERE company_id = ?', [userId, companyId])
-      ));
+        // Update Address
+        if (address) {
+            await connection.query('UPDATE address SET street = ?, number = ?, cep = ?, additional_info = ? WHERE user_id = ?', [address.street, address.number, address.cep, address.additional_info, userId]);
+        }
 
-      await connection.commit();
+        // Add Companies
+        if (companiesToAdd) {
+            for (const company of companiesToAdd) {
+                const cnpjCheck = await connection.query('SELECT company_id FROM company WHERE cnpj = ? AND user_id <> ?', [company.cnpj, userId]);
+                if (cnpjCheck.company_id != null) {
+                    await connection.rollback();
+                    return res.status(400).json({ error: 'CNPJ already in use' });
+                } else {
+                    // Adicionar nova empresa
+                    await connection.query('INSERT INTO company (user_id, name, cnpj, industry, employees_count, revenue) VALUES (?, ?, ?, ?, ?, ?)',
+                        [userId, company.name, company.cnpj, company.industry, company.employees_count, company.revenue]);
+                }
+            }
+        }
 
-      res.status(201).json({ userId, addressId, companyIds });
+        // Remove Companies
+        if (companiesToRemove && companiesToRemove.length > 0) {
+            for (const companyId of companiesToRemove) {
+                await connection.query('DELETE FROM company WHERE company_id = ? AND user_id = ?', [companyId, userId]);
+            }
+        }
+
+        await connection.commit();
+
+        res.status(200).json({ message: 'User updated successfully' });
     } catch (error) {
-      await connection.rollback();
-      throw error;
+        await connection.rollback();
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     } finally {
-      connection.release();
+        connection.release();
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
 });
+
+
+
+
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+// request
